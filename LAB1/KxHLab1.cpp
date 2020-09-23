0d3610533c3c2bec981d578e7f443c6b9b855786
LAbeast021 <kiarashhesampour@gmail.com>
2020-09-23 09:39:02 -0700

    lab1 done

--- LAB1/KxHLab1.cpp
+++ LAB1/KxHLab1.cpp
@@ -8,13 +8,14 @@ using namespace std;
 
 int main(){
 
-    unsigned long long  operand_1, operand_2, calculated_result; 
+    unsigned long long  operand_1, operand_2;
     char rep = 'y', user_option; 
-
 
     cout << "Bitwise Operation Calculator - CS130 - 64 bits - Kiarash Hesampour" << endl;
 
-    while(rep != 'n'){
+    while(rep == 'y'){
+        
+
         cout
         <<"Enter one of the following choices: " << endl
         <<"& - AND" << endl
@@ -32,7 +33,8 @@ int main(){
         catch(...){
             cout << "Input error, enter choice and values again." <<endl;
         }
-        if ((user_option!='&') && (user_option!='|') && (user_option!='^') && (user_option!='~') && (user_option!='-') && (user_option!='<') && (user_option!='>') && (user_option!='#')){
+
+        if ((user_option!='~') && (user_option!='<') && (user_option!='>') && (user_option!='-') && (user_option!='#') && (user_option!='^') && (user_option!='|') && (user_option!='&')){
             cout << "Invalid choice " << user_option << ", please try again. " << endl ;
         }
         else {
@@ -42,59 +44,65 @@ int main(){
             }
             else {
                 switch(user_option){
-           case '&' :
-               cout<<"Enter values for first and second operand:\n";
-               cin >> operand_1 >> operand_2;
-               cout<<"Result:"<<(operand_1&operand_2)<<" [Hexadecimal:"<<hex<<(operand_1&operand_2)<<"]\n";
-               break;
-           case '|' :
-               cout<<"Enter values for first and second operand:\n";
-               cin >> operand_1 >> operand_2;
-               cout<<"Result:"<<(operand_1 | operand_2)<<" [Hexadecimal:"<<hex<<(operand_1 | operand_2)<<"]\n";
-               break;
-           case '^' :
-               cout<<"Enter values for first and second operand:\n";
-               cin >> operand_1 >> operand_2;
-               cout<<"Result:"<<(operand_1^ operand_2)<<" [Hexadecimal:"<<hex<<(operand_1^ operand_2)<<"]\n";
-               break;
-           case '!' :
-               cout<<"Enter value for operand:\n";
-               cin>> operand_1;
-               cout<<"Result:"<<(!operand_1)<<" [Hexadecimal:"<<hex<<(!operand_1)<<"]\n";
-               break;
-           case '~' :
-               cout<<"Enter value for operand:\n";
-               cin>> operand_1;
-               cout<<"Result:"<<(~operand_1)<<" [Hexadecimal:"<<hex<<(~operand_1)<<"]\n";
-               break;
-           case '<' :
-               cout<<"Enter values for first value and shift value:\n";
-               cin >> operand_1 >> operand_2;
-               if(operand_2 > sizeof(operand_1) * 8 ){
-                   cout<<"Shift value "<< operand_2 <<" exceeds bit size "<<sizeof(operand_1 ) * 8;
+                        case '&' :
+                            cout << "Enter values for first and second operand:" << endl;
+                            cin >> operand_1 >> operand_2;
+                            cout << "Result:" <<(operand_1 & operand_2) << " [Hexadecimal:" << hex << (operand_1 & operand_2) <<"]" << endl;
+                            break;
+                            
+                        case '|' :
+                            cout << "Enter values for first and second operand: " << endl;
+                            cin >> operand_1 >> operand_2;
+                            cout << "Result:"<< (operand_1 | operand_2) << " [Hexadecimal:"<< hex << (operand_1 | operand_2) <<"]" << endl;
+                            break;
 
-               }
-               else{
-                   cout<<"Result:"<<(operand_1<< operand_2)<<" [Hexadecimal:"<<hex<<(operand_1<< operand_2)<<"]\n";
-               }
-               break;
-           case '>' :
-               cout<<"Enter values for first value and shift value:\n";
-               cin >> operand_1 >> operand_2;
-               if(operand_2>sizeof(operand_1)*8){
-                   cout<<"Shift value "<<operand_2<<" exceeds bit size "<<sizeof(operand_1)*8;
-                  
-               }
-                   else{
-                   cout<<"Result:"<<(operand_1>> operand_2)<<" [Hexadecimal:"<<hex<<(operand_1>> operand_2)<<"]\n";
-               }
-               break;
+                        case '^' :
+                            cout << "Enter values for first and second operand:" << endl;
+                            cin >> operand_1 >> operand_2;
+                            cout << "Result:" << (operand_1 ^ operand_2) <<" [Hexadecimal:" << hex <<(operand_1 ^ operand_2) <<"]" << endl;
+                            break;
+
+                        case '~' :
+                            cout << "Enter value for operand:" << endl;
+                            cin >> operand_1;
+                            cout << "Result:" << (~operand_1) << " [Hexadecimal:" << hex << (~operand_1) << "]" << endl;
+                            break;
+
+                        case '-' :
+                            cout << "Enter value for operand:" << endl;
+                            cin >> operand_1;
+                            cout << "Result:"<<(-operand_1) << " [Hexadecimal:" << hex << (-operand_1) <<"]" << endl;
+                            break;
+
+                        case '<' :
+                            cout << "Enter values for first and second operand" << endl;
+                            cin >> operand_1 >> operand_2;
+                            if(operand_2 > sizeof(operand_1) * 8 ){
+                                cout << "Shift operand (" << operand_2 << ") bannot exceed number of bits  (" << sizeof(operand_1) * 8 << ")  - setting result to zero." << endl;
+                                cout << "Result: 0 [Hexadecimal: 0]" << endl ;
+                            }
+                            else{
+                                cout << "Result:"<<(operand_1 << operand_2) <<" [Hexadecimal:" << hex << (operand_1 << operand_2) <<"]" <<endl;
+                            }
+                            break;
+                        case '>' :
+                            cout << "Enter values for first and second operand" << endl;
+                            cin >> operand_1 >> operand_2;
+                            if(operand_2 > sizeof(operand_1) * 8){
+                                cout << "Shift operand ("<< operand_2 <<") bannot exceed number of bits  (" << sizeof(operand_1) * 8 << ")  - setting result to zero." << endl;
+                                cout << "Result: 0 [Hexadecimal: 0]" << endl ;
+                                
+                            }
+                                else{
+                                cout << "Result:" << (operand_1 >> operand_2) << " [Hexadecimal:" << hex << (operand_1 >> operand_2) <<"]" << endl;
+                            }
+                            break;
        }
             }
 
         
                     
     }
-    return 0;
 }
+    return 0;
 }
